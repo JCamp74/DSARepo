b6cff53441e2ade0dcd997dc6fb1f754153b14e6
JCamp74 <jbcampbell10@yahoo.com>
2024-12-09 06:05:42 -0500

    Made note to update file location before use.

--- Stock_Bot_Project/NewProject4/src/Main.java
+++ Stock_Bot_Project/NewProject4/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         StockBot sb = new StockBot();
-        sb.run();
+        sb.run(); //MUST CHANGE THE FILE LOCATIONS BEFORE RUNNING, WILL NOT WORK OTHERWISE!!!! (Thanks!)
     }
 }
